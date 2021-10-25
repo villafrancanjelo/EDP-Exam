c7f5411fcbbc94baff34ce930f814731a94d0119
villafrancanjelo <jelo.villafranca@gmail.com>
2021-10-25 18:57:53 +0800

    changes in app.components

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit  {
+
+  currentNav: string = '';
+  observer: any;
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navClick(current:string){
+    this.currentNav = current;
+  }
+  title='profile';
+
+}
---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/Icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import { MatDividerModule } from '@angular/material/divider';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+
+
+const routes: Routes = [
+  {path: 'App', component: AppComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'contact', component: ContactComponent},
+
+
+{path: "**", redirectTo: 'Profile'}
+]
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    GalleryComponent,
+    ContactComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatDividerModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
