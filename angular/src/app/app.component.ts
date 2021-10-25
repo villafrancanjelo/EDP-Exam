1dca713056914fa40fe34a39faf44d0b90673c8b
villafrancanjelo <jelo.villafranca@gmail.com>
2021-10-25 19:10:55 +0800

    Changes for App.Component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component,OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
